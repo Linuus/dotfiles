367d6e6053ee64d889e5c95003a9930b3522e248
Linus Pettersson <linus.pettersson@gmail.com>
2017-01-24 21:33:53 +0100

    Update shell configs

--- zsh/slim.zsh
+++ zsh/slim.zsh
@@ -23,4 +23,3 @@ source $slim_path/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 if command -v fasd >/dev/null 2>&1; then
   eval "$(fasd --init zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install posix-alias)"
 fi
-

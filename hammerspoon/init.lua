d82511102b96b0646561f47f90fdac1d18338ba1
Linus Pettersson <linus.pettersson@gmail.com>
2017-02-22 21:31:31 +0100

    Add hammerspoon config

---
+++ hammerspoon/init.lua
@@ -0,0 +1,103 @@
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
+    hs.reload()
+end)
+hs.alert.show("Config loaded")
+
+hs.hotkey.bind({"cmd", "alt"}, "Left", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt"}, "Right", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x + (max.w / 2)
+    f.y = max.y
+    f.w = max.w / 2
+    f.h = max.h
+    win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt"}, "f", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x
+    f.y = max.y
+    f.w = max.w
+    f.h = max.h
+    win:setFrame(f)
+end)
+
+-- control is escape on tap --
+
+send_escape = false
+last_mods = {}
+
+control_key_handler = function()
+  send_escape = false
+end
+
+control_key_timer = hs.timer.delayed.new(0.15, control_key_handler)
+
+control_handler = function(evt)
+  local new_mods = evt:getFlags()
+  if last_mods["ctrl"] == new_mods["ctrl"] then
+    return false
+  end
+  if not last_mods["ctrl"] then
+    last_mods = new_mods
+    send_escape = true
+    control_key_timer:start()
+  else
+    if send_escape then
+      hs.eventtap.keyStroke({}, "ESCAPE")
+    end
+    last_mods = new_mods
+    control_key_timer:stop()
+  end
+  return false
+end
+
+control_tap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, control_handler)
+control_tap:start()
+
+other_handler = function(evt)
+  send_escape = false
+  return false
+end
+
+other_tap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, other_handler)
+other_tap:start()
+
+-- simulate flux
+
+-- hs.location.start()
+-- hs.timer.doAfter(1, function()
+--                    loc = hs.location.get()
+--                    hs.location.stop()
+
+--                    local times = {sunrise = "07:00", sunset = "20:00"}
+
+--                    if loc then
+--                      local tzOffset = tonumber(string.sub(os.date("%z"), 1, -3))
+--                      for i,v in pairs({"sunrise", "sunset"}) do
+--                        times[v] = os.date("%H:%M", hs.location[v](loc.latitude, loc.longitude, tzOffset))
+--                      end
+--                    end
+
+--                    hs.redshift.start(3600, times.sunset, times.sunrise)
+-- end)

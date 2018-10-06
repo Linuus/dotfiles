8a113753ee429acd45a232e4467a4fb0a45cd6b0
Linus Marton <linus.pettersson@gmail.com>
2018-10-06 13:57:03 +0200

    Add window switcher

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -79,6 +79,24 @@ hs.hotkey.bind({"cmd", "alt"}, "f", function()
     win:setFrame(f, 0)
 end)
 
+-- window switcher
+-- works best if only one space is used, otherwise ordering is messed up sometimes.
+-- TODO: try to fix ordering between spaces.
+switcher = hs.window.switcher.new() -- default windowfilter: only visible windows, all Spaces
+switcher.ui.textColor = {0.9 ,0.9, 0.9}
+switcher.ui.highlightColor = {0.4,0.4,0.4,0.7}
+switcher.ui.backgroundColor = {0.4, 0.4, 0.4, 0.6}
+switcher.ui.titleBackgroundColor = {0, 0, 0, 0}
+switcher.ui.showThumbnails = false
+switcher.ui.showSelectedThumbnail = false
+switcher.ui.fontName = "System"
+switcher.ui.textSize = 14
+switcher.ui.showSelectedTitle = false
+
+-- bind to hotkeys; WARNING: at least one modifier key is required!
+hs.hotkey.bind("alt","tab",function()switcher:next()end)
+hs.hotkey.bind("alt-shift","tab",function()switcher:previous()end)
+
 -- defeat paste block
 
 hs.hotkey.bind({"cmd", "alt"}, "v", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)

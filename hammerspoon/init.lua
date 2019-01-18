ad72b2edd199d5f604abad6c6404f18bcf38edb5
Linus Marton <linus.pettersson@gmail.com>
2019-01-18 09:22:42 +0100

    Add Firefox and Emacs launchers

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,5 +1,6 @@
 local hyper = {"cmd", "alt"}
 local launcher = {"cmd", "ctrl"}
+local launcher_2 = {"cmd", "ctrl", "shift"}
 hs.window.animationDuration = 0
 
 hs.loadSpoon("SpoonInstall")
@@ -20,15 +21,46 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
 end)
 hs.alert.show("Config loaded")
 
-hs.hotkey.bind(launcher, "Return", function()
+hs.hotkey.bind(launcher, "e", function()
+   hs.osascript.applescript([[
+      on emacsRunning()
+         tell application "System Events" to (name of processes) contains "emacs"
+      end emacsRunning
+
+      on run argv
+
+      if (emacsRunning() = false) then
+         do shell script "open -a emacs " & (item 1 of argv)
+      else
+         tell application "emacs" to activate
+      end if
+      end run
+   ]])
+end)
+
+hs.hotkey.bind(launcher_2, "e", function()
   hs.execute("open -n -a emacs", true)
+end)
+
+hs.hotkey.bind(launcher, "f", function()
+   hs.osascript.applescript([[
+      on firefoxRunning()
+         tell application "System Events" to (name of processes) contains "Firefox"
+      end firefoxRunning
+
+      on run argv
+
+      if (firefoxRunning() = false) then
+         do shell script "open -a Firefox " & (item 1 of argv)
+      else
+         tell application "Firefox" to activate
+      end if
+      end run
+   ]])
 end)
 
 hs.hotkey.bind(launcher, "b", function()
   hs.execute("open -a qutebrowser", true)
-end)
-hs.hotkey.bind(launcher, "b", function()
-  hs.execute("open -n -a qutebrowser", true)
 end)
 
 -- window switcher
@@ -53,8 +85,18 @@ hs.hotkey.bind("alt-shift","tab",function()switcher:previous()end)
 hs.hotkey.bind(hyper, "v", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
 
 -- volume
-hs.hotkey.bind(hyper, "]", function() hs.audiodevice.defaultOutputDevice():setVolume(hs.audiodevice.current().volume + 5) end)
-hs.hotkey.bind(hyper, "[", function() hs.audiodevice.defaultOutputDevice():setVolume(hs.audiodevice.current().volume - 5) end)
+hs.hotkey.bind(
+   hyper,
+   "]",
+   function()
+      hs.audiodevice.defaultOutputDevice():setVolume(hs.audiodevice.current().volume + 5)
+end)
+hs.hotkey.bind(
+   hyper,
+   "[",
+   function()
+      hs.audiodevice.defaultOutputDevice():setVolume(hs.audiodevice.current().volume - 5)
+end)
 
 -- spotify
 hs.hotkey.bind(hyper, "c", hs.spotify.displayCurrentTrack)

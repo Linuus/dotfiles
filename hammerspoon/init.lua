d35ccff6f289de0c001d940cdec83cf88430ecdd
Linus Marton <linus.pettersson@gmail.com>
2019-10-09 08:30:38 +0200

    Remove window switcher
    Never used and broke in a Hammperspoon update

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -63,24 +63,6 @@ hs.hotkey.bind(launcher, "b", function()
   hs.execute("open -a qutebrowser", true)
 end)
 
--- window switcher
--- works best if only one space is used, otherwise ordering is messed up sometimes.
--- TODO: try to fix ordering between spaces.
-switcher = hs.window.switcher.new() -- default windowfilter: only visible windows, all Spaces
-switcher.ui.textColor = {1, 1, 1}
-switcher.ui.highlightColor = {0.4, 0.4, 0.4, 0.7}
-switcher.ui.backgroundColor = {0.4, 0.4, 0.4, 0.6}
-switcher.ui.titleBackgroundColor = {0, 0, 0, 0}
-switcher.ui.showThumbnails = false
-switcher.ui.showSelectedThumbnail = false
-switcher.ui.fontName = "System"
-switcher.ui.textSize = 14
-switcher.ui.showSelectedTitle = false
-
--- bind to hotkeys; WARNING: at least one modifier key is required!
-hs.hotkey.bind("alt","tab",function()switcher:next()end)
-hs.hotkey.bind("alt-shift","tab",function()switcher:previous()end)
-
 -- defeat paste block
 hs.hotkey.bind(hyper, "v", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
 

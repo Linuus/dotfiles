574ef1f1609bd329adcfc18650365bd28ba01ff6
Linus Marton <linus.pettersson@gmail.com>
2020-01-16 13:34:15 +0100

    Update surfingkeys config

--- surfingkeys.js
+++ surfingkeys.js
@@ -1,10 +1,16 @@
 map("<Ctrl-u>", "u");
 map("<Ctrl-d>", "d");
+unmap("u");
+unmap("d");
 
 map("H", "S");
 map("L", "D");
 map("J", "R");
 map("K", "E");
+unmap("S");
+unmap("D");
+unmap("R");
+unmap("E");
 
 cmap("<Ctrl-j>", "<Tab>");
 cmap("<Ctrl-k>", "<Shift-Tab>");
